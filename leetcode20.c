234a6d4bfe1e2e65b4fc4854518198537baa09bb
LiuHendrix <as6814123@gmail.com>
2024-04-22 02:36:34 +0800

    init commit

---
+++ leetcode20.c
@@ -0,0 +1,47 @@
+bool isValid(char* s) {
+    char stack[10000];
+    int top = 0;
+    for (int i=0; i<strlen(s); i++) {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+            stack[top] = s[i];
+            top++;
+        }
+        else if (s[i] == ')') {
+            if (top == 0) {
+                return false;
+            }
+            else if (stack[top-1] == '(') {
+                top--;
+            }
+            else {
+                return false;
+            }
+        }
+        else if (s[i] == ']') {
+            if (top == 0) {
+                return false;
+            }
+            else if (stack[top-1] == '[') {
+                top--;
+            }
+            else {
+                return false;
+            }
+        }
+        else if (s[i] == '}') {
+            if (top == 0) {
+                return false;
+            }
+            else if (stack[top-1] == '{') {
+                top--;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    if (top!=0) {
+        return false;
+    }
+    return true;  
+}
